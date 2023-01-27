8590c5d01dbb924ed45e61b33069ce35d2449b96
mariano quiroga <mariano.eq@gmail.com>
2023-01-28 00:43:59 +0100

    ts files changed

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,7 @@ let dead: number = 0;
 
 let cells: number[][] = [
   [0, 1, 0],
-  [0, 0, 1],
+  [0, 1, 0],
   [1, 1, 1],
 ];
 console.table(cells);
-
-for (let i = 0; i < cells.length; i++) {}
-
-//
