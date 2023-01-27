81432e116723068692e185b3e3417c91e3717654
mariano quiroga <mariano.eq@gmail.com>
2023-01-28 00:40:32 +0100

    ts files added

--- dist/main.js
+++ dist/main.js
@@ -1,1 +1,11 @@
 "use strict";
+let alive = 1;
+let dead = 0;
+let cells = [
+    [0, 1, 0],
+    [0, 0, 1],
+    [1, 1, 1],
+];
+console.table(cells);
+for (let i = 0; i < cells.length; i++) { }
+//
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,13 @@
+let alive: number = 1;
+let dead: number = 0;
 
+let cells: number[][] = [
+  [0, 1, 0],
+  [0, 0, 1],
+  [1, 1, 1],
+];
+console.table(cells);
+
+for (let i = 0; i < cells.length; i++) {}
+
+//
