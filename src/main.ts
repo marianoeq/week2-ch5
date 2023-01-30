a95bc625293508e6305a2e62690d9431f16334eb
mariano quiroga <mariano.eq@gmail.com>
2023-01-30 01:06:27 +0100

    game of life workflow done

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,137 @@
 let alive: number = 1;
 let dead: number = 0;
 
-let cells: number[][] = [
-  [0, 1, 0],
-  [0, 1, 0],
-  [1, 1, 1],
-];
-console.table(cells);
+let deathCellsInContactAlive: number = 0;
+let aliveCellsInContactAlive: number = 0;
 
-// if dead cell is in contact with one or two alive cell, it keeps dead.
-// if dead cell is in contact with 3 alive cells, it turns alive.
-// if dead cell is in contact with more than 3 alive cells, it keeps dead.
+function checkCellsHorizontally(
+  cellsBoard: number[][],
+  currentCell: number | null
+) {
+  for (let i = 0; i < cellsBoard.length; i++) {
+    for (let j = 0; j < cellsBoard.length; j++) {
+      if (cellsBoard[i][j] === alive) {
+        currentCell = alive;
+        if (
+          currentCell === cellsBoard[i][j + 1] &&
+          cellsBoard[i][j + 1] !== undefined
+        )
+          aliveCellsInContactAlive++;
+        if (
+          currentCell === cellsBoard[i][j - 1] &&
+          cellsBoard[i][j - 1] !== undefined
+        )
+          aliveCellsInContactAlive++;
+      }
+
+      if (cellsBoard[i][j] === dead) {
+        currentCell = dead;
+        if (
+          currentCell !== cellsBoard[i][j + 1] &&
+          cellsBoard[i][j + 1] !== undefined
+        )
+          deathCellsInContactAlive++;
+        if (
+          currentCell !== cellsBoard[i][j - 1] &&
+          cellsBoard[i][j - 1] !== undefined
+        )
+          deathCellsInContactAlive++;
+      }
+      if (currentCell === alive) {
+        if (i < cellsBoard.length - 1) checkDown(cellsBoard, i, currentCell);
+        if (i > 0) checkUp(cellsBoard, i, currentCell);
+      }
+      if (currentCell === dead) {
+        if (i < cellsBoard.length - 1) checkDown(cellsBoard, i, currentCell);
+        if (i > 0) checkUp(cellsBoard, i, currentCell);
+      }
+    }
+  }
+}
+
+function checkDown(cellsBoard: number[][], idx: number, currentCell: number) {
+  for (let i = idx; i < cellsBoard.length; i++) {
+    for (let j = 0; j < cellsBoard.length; j++) {
+      if (currentCell === alive) {
+        if (
+          currentCell === cellsBoard[i][j + 1] &&
+          cellsBoard[i][j + 1] !== undefined
+        )
+          aliveCellsInContactAlive++;
+        if (
+          currentCell === cellsBoard[i][j - 1] &&
+          cellsBoard[i][j - 1] !== undefined
+        )
+          aliveCellsInContactAlive++;
+
+        if (currentCell === dead) {
+          if (
+            currentCell !== cellsBoard[i][j + 1] &&
+            cellsBoard[i][j + 1] !== undefined
+          )
+            deathCellsInContactAlive++;
+          if (
+            currentCell !== cellsBoard[i][j - 1] &&
+            cellsBoard[i][j - 1] !== undefined
+          )
+            deathCellsInContactAlive++;
+        }
+      }
+    }
+  }
+}
+
+function checkUp(cellsBoard: number[][], idx: number, currentCell: number) {
+  for (let i = idx; i < cellsBoard.length; i++) {
+    for (let j = 0; j < cellsBoard.length; j++) {
+      if (currentCell === alive) {
+        if (
+          currentCell === cellsBoard[i][j + 1] &&
+          cellsBoard[i][j + 1] !== undefined
+        )
+          aliveCellsInContactAlive++;
+        if (
+          currentCell === cellsBoard[i][j - 1] &&
+          cellsBoard[i][j - 1] !== undefined
+        )
+          aliveCellsInContactAlive++;
+      }
+
+      if (currentCell === dead) {
+        if (
+          currentCell !== cellsBoard[i][j + 1] &&
+          cellsBoard[i][j + 1] !== undefined
+        )
+          deathCellsInContactAlive++;
+        if (
+          currentCell !== cellsBoard[i][j - 1] &&
+          cellsBoard[i][j - 1] !== undefined
+        )
+          deathCellsInContactAlive++;
+      }
+    }
+  }
+}
+
+function gameOfLife() {
+  let cellsBoard: number[][] = [
+    [alive, alive, alive],
+    [dead, dead, dead],
+    [dead, dead, dead],
+  ];
+
+  let currentCell: number | null = null;
+  checkCellsHorizontally(cellsBoard, currentCell);
+  if (deathCellsInContactAlive === 3) {
+    currentCell = alive;
+  }
+  if (aliveCellsInContactAlive <= 1) {
+    currentCell = dead;
+  }
+  if (aliveCellsInContactAlive === 2 || aliveCellsInContactAlive === 3) {
+    currentCell;
+  }
+  if (aliveCellsInContactAlive >= 4) {
+    currentCell = dead;
+  }
+}
